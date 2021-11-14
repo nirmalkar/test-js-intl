fe1b83162158728be111c3104530396166dfda05
Hemant Nirmalkar <nirmalkar@Hemants-MacBook-Pro.local>
2021-11-14 12:44:00 +0530

    test-js-intl initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import { translation } from './translation.js'
+import { jsIntl } from 'js-intl'
+
+jsIntl(translation, 'fr')
---
+++ translation.js
@@ -0,0 +1,30 @@
+export const translation = {
+    en: {
+        strings: {
+            'testPage.greetings': 'Hello there! How are you?',
+            'testPage.intro':
+                'My name is Hemant Nirmalkar and I am from Bilaspur Chhattisgarh, As for my hobbies I like to watch web Series and create cool stuff.',
+        },
+    },
+    es: {
+        strings: {
+            'testPage.greetings': '¡Hola! ¿Cómo estás?',
+            'testPage.intro':
+                'Mi nombre es Hemant Nirmalkar y soy de BilaspurChhattisgarh, En cuanto a mis pasatiempos, me gusta ver series web y crear cosas interesantes.',
+        },
+    },
+    fr: {
+        strings: {
+            'testPage.greetings': 'Bonjour! Comment ca va?',
+            'testPage.intro':
+                "Je m'appelle Hemant Nirmalkar et je viens de Bilaspur Chhattisgarh, En ce qui concerne mes passe-temps, j'aime regarder des séries Web et créer des trucs sympas.",
+        },
+    },
+    pt: {
+        strings: {
+            'testPage.greetings': 'Olá! Como você está?',
+            'testPage.intro':
+                'Meu nome é Hemant Nirmalkar e sou de Bilaspur Chhattisgarh, Quanto aos meus hobbies, gosto de assistir séries na web e criar coisas legais.',
+        },
+    },
+}
